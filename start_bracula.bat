d1d3136087103512327410390886ee71a44c7042
Reduan Nur Labid <105357493+ReduanNurLabid@users.noreply.github.com>
2025-05-11 01:18:07 +0600

    updated start bracula bat file

--- start_bracula.bat
+++ start_bracula.bat
@@ -12,11 +12,21 @@ if %errorLevel% == 0 (
     exit
 )
 
-REM Set paths
+REM === Configuration ===
+REM Set the path to your XAMPP installation folder here:
 set XAMPP_PATH=C:\xampp
+
+REM Set the port your Apache server uses (default 8081)
+set APACHE_PORT=8081
+
+REM Set paths based on XAMPP_PATH
 set PHP_PATH=%XAMPP_PATH%\php\php.exe
 set MYSQL_PATH=%XAMPP_PATH%\mysql\bin\mysql.exe
-set PROJECT_PATH=%XAMPP_PATH%\htdocs\BRACULA
+set PROJECT_PATH=%XAMPP_PATH%\htdocs\bracula
+
+echo Using XAMPP path: %XAMPP_PATH%
+echo Using project path: %PROJECT_PATH%
+echo Using Apache port: %APACHE_PORT%
 
 echo Starting XAMPP services...
 net start MySQL
@@ -35,18 +45,23 @@ echo Running database setup scripts...
 echo Populating initial data...
 "%PHP_PATH%" "%PROJECT_PATH%\database\populate_users.php"
 "%PHP_PATH%" "%PROJECT_PATH%\database\populate_resources.php"
-"%PHP_PATH%" "%PROJECT_PATH%\database\add_test_user.php"
+
+REM Optional: Add test user if the script exists
+if exist "%PROJECT_PATH%\database\add_test_user.php" (
+    echo Adding test user...
+    "%PHP_PATH%" "%PROJECT_PATH%\database\add_test_user.php"
+)
 
 echo Verifying database setup...
 "%PHP_PATH%" "%PROJECT_PATH%\database\check_users.php"
 "%PHP_PATH%" "%PROJECT_PATH%\database\check_resources.php"
 
 echo Opening BRACULA in your default browser...
-start http://localhost:8081/bracula/
+start http://localhost:%APACHE_PORT%/bracula/
 
 echo.
 echo BRACULA is now running!
-echo You can access the site at: http://localhost:8081/bracula/
+echo You can access the site at: http://localhost:%APACHE_PORT%/bracula/
 echo.
 echo Press any key to close this window...
-pause >nul 
+pause >nul
