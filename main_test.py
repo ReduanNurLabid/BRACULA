01e386864ce4729de1dd0cd7a851c54d84c374a3
Jerin022846 <jerin.khan@g.bracu.ac.bd>
2025-05-17 22:35:09 +0600

    update main

--- main_test.py
+++ main_test.py
@@ -8,6 +8,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from test_login import TestLogin
 from test_resources import TestResources
 from test_feeds import TestFeeds
+from test_accommodation import TestAccommodation
 
 
 @pytest.fixture(scope="module")
@@ -25,24 +26,34 @@ def test_login(setup):
     user_login = TestLogin()
     user_login.login_valid(setup)
 
-#Test the Resources page
+# Test the Resources page
 def test_resources_page(setup):
      upload_resources = TestResources()
      upload_resources.resources_page(setup)
-#Test the download functionality
+# Test the download functionality
 def test_download_resources(setup):
      download_resources = TestResources()
      download_resources.download_resources(setup)
 # Test the search functionality
-# def test_search_resources(setup):
+def test_search_resources(setup):
      search_resources = TestResources()
      search_resources.search_resources(setup)
-#Test the Feeds page
+# Test the Feeds page
 def test_feeds_page(setup):
      feeds = TestFeeds()
      feeds.feeds_page(setup)
 
-#Test the comment functionality
+# Test the comment functionality
 def test_comment_on_post(setup):
-    comment = TestFeeds()
-    comment.comment_on_post(setup)
+     comment = TestFeeds()
+     comment.comment_on_post(setup)
+
+# Test the Accommodation page
+def test_accommodation_page(setup):
+     accommodation = TestAccommodation()
+     accommodation.accommodation_page(setup)
+
+# Test the filter functionality
+def test_filter_accommodation(setup):
+    filter_accommodation = TestAccommodation()
+    filter_accommodation.filter_accommodation(setup)
